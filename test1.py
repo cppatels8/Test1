db40b4e9a0e8eaeccc0ed215982ebfc9ef8210fc
Chandan Patel <chandan@Chandan-Patel.local>
2020-03-02 12:56:39 +0530

    Added test1

---
+++ test1.py
@@ -0,0 +1,17 @@
+import os
+# import logging
+
+# from logging.handlers import RotatingFileHandler
+from test import app as application
+
+# application.logger.setLevel(logging.INFO)
+# handler = RotatingFileHandler('application.log',
+#                                maxBytes=10000, backupCount=5)
+# formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# application.logger.addHandler(handler)
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
